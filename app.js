7fba341161987692e4dbf005905deff80203aebe
suuum12 <57670228+suuum12@users.noreply.github.com>
2020-03-17 23:56:32 +0900

    0317 로그인 수정

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ const crawler = async () => {
       width: 1080,
       height: 1080,
     });
-    await page.goto('https://instagram.com');
-    if (await page.$('a[href="/nutee.skhu.2020/"]')) {
+    await page.goto('https://instagram.com/');
+    if (await page.$('a[href="/eunwoo/"]')) {
       console.log('이미 로그인 되어 있습니다.');
     } else {
       await page.waitForSelector('input[name="username"]');
@@ -32,42 +32,40 @@ const crawler = async () => {
     let prevPostId = '';
     
     const newPost = await page.evaluate(() => {
-        const article = document.querySelector('article:first-child');
-        const postId = article.querySelector('.c-Yi7') && article.querySelector('.c-Yi7').href.split('/').slice(-2, -1)[0];
-        const img = article.querySelector('.KL4Bh img') && article.querySelector('.KL4Bh img').src;
-        return {
+      const article = document.querySelector('article:first-child');
+      const postId = article.querySelector('.c-Yi7') && article.querySelector('.c-Yi7').href.split('/').slice(-2, -1)[0];
+      const img = article.querySelector('.KL4Bh img') && article.querySelector('.KL4Bh img').src;
+      return {
           postId, img,
-        }
-      });
+      }
+  });
 
-      if (newPost.postId !== prevPostId) {
-        console.log(newPost);
-        if (!result.find((v) => v.postId === newPost.postId)) {
+  if (newPost.postId !== prevPostId) {
+      console.log(newPost);
+      if (!result.find((v) => v.postId === newPost.postId)) {
           const exist = await db.Instagram.findOne({ where: { postId: newPost.postId } });
           if (!exist) {
-            result.push(newPost);
+              result.push(newPost);
           }
-        }
       }
-      await page.waitFor(1000);
+  }
+  await page.waitFor(1000);
 
-      prevPostId = newPost.postId;
+  prevPostId = newPost.postId;
 
-      await page.waitFor(1000);
-      
-      await Promise.all(result.map((r) => {
-        return db.Instagram.create({
+  await Promise.all(result.map((r) => {
+      return db.Instagram.create({
           postId: r.postId,
           media: r.img,
-        });
-      }));
+      });
+  }));
 
-    console.log('성공');
-    await page.close();
-    await browser.close();
-  } catch (e) {
-    console.error(e);
-  }
+  console.log('성공');
+  await page.close();
+  await browser.close();
+} catch (e) {
+  console.error(e);
+}
 };
 
-crawler();
+module.exports = crawler();
